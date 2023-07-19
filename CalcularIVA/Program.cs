346cc54691308ad556cf21cf9c13053ef67386b8
rhernandezch <rhernandezch@yahoo.com>
2023-07-19 08:47:40 -0600

    adding final samples

---
+++ CalcularIVA/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalcularIVA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public const int IVA = 12;
+            decimal viajemadrid = 9231.57d;
+        }
+    }
+}
